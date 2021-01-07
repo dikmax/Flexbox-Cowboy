341f91d3ed11e95ac33b1d53ef0ebb856675f0af
PavelVLG <pavelvlg4@mail.ru>
2021-01-07 17:37:24 +0300

    master

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <div className="navigation">навигация</div>
+      </div>
+      <div className="content_left">
+        <div>Контент слева</div>
+      </div>
+      <div className="content_rite">
+        <div>Гриид</div>
+      </div>
+      <div className="footer"><div>Подвал</div></div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootswatch/dist/sketchy/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
