c1da1b1dfb7b6acc3f99f4cbd35fd3672cd2ab74
PavelVLG <pavelvlg4@mail.ru>
2021-01-15 14:19:41 +0300

    master

--- src/ContenteynerRiteAndLeft/ContenteynerRiteAndLeft.js
+++ src/ContenteynerRiteAndLeft/ContenteynerRiteAndLeft.js
@@ -9,28 +9,28 @@ class ContenteynerRiteAndLeft extends React.Component{
     constructor(props){
       super(props);
       this.state = {
-        FlexMoving:  null   
+        flexMoving:  ([
+          ['description',  `Добро пожаловать в ... Игра, в которой тебе нужно ... и его друзьям написанием CSS кода!  Направь этого ... на лилию справа используя свойство justify-content, которое выравнивает элементы горизонтально и принимает следующие значения:`],
+          ['ulElement', [
+           ['flex-start Элементы выравниваются по левой стороне контейнера.'],
+           ['flex-end Элементы выравниваются по правой стороне контейнера.'],
+           ['center Элементы выравниваются по центру контейнера.'],
+           ['space-between Элементы отображаются с одинаковыми отступами между ними.'],
+           ['space-around Элементы отображаются с одинаковыми отступами вокруг них.'],
+          ]],
+     { display: 'flex',
+       justifyContent: 'center',
+     },
+    ])
       }};
-        level1 = ([
-            ['description',  `Добро пожаловать в ... Игра, в которой тебе нужно ... и его друзьям написанием CSS кода!  Направь этого ... на лилию справа используя свойство justify-content, которое выравнивает элементы горизонтально и принимает следующие значения:`],
-            ['ulElement', [
-             ['flex-start Элементы выравниваются по левой стороне контейнера.'],
-             ['flex-end Элементы выравниваются по правой стороне контейнера.'],
-             ['center Элементы выравниваются по центру контейнера.'],
-             ['space-between Элементы отображаются с одинаковыми отступами между ними.'],
-             ['space-around Элементы отображаются с одинаковыми отступами вокруг них.'],
-            ]],
-       { display: 'flex',
-         justifyContent: 'center',
-       },
-      ]); 
+          
      
       
   render(){
     return (
       <>
-        <LeftContent map ={this.level1}/>
-        <RiteContent map ={this.level1}/>
+        <LeftContent map ={this.state.flexMoving}/>
+        <RiteContent map ={this.state.flexMoving}/>
       </>
     );
   } };
