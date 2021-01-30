8486e2f51fcb8532610ee5ebf1ac21ed4028da80
PavelVLG <pavelvlg4@mail.ru>
2021-01-30 17:22:05 +0300

    master

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,13 +1,13 @@
 import './Navigation.css';
 function Navigation() {
   return (
-      <div className="header">
-        <div className="header_navigation">
-            <div className="header_navigation_logo"><h1>Cowboy</h1></div>
-            <div className="header_navigation_menu"><h2 className="header_navigation_element">language</h2></div>
-            <div className="header_navigation_menu"><h2 className="header_navigation_element">difficalty</h2></div>
-        </div>
-    </div>
+  <nav className="header"> 
+    <div className="logo"><img src="#" alt="logo"/></div> 
+    <div className="header__conteyner-flex">
+      <div className="header__item"><p>language</p></div>
+      <div className="header__item"><p>theme</p></div>
+    </div> 
+  </nav>
   );
 }
 
