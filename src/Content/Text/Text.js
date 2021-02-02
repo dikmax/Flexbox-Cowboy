5c25c419f77d4770792c49ea8982f3fcc83d910f
PavelVLG <pavelvlg4@mail.ru>
2021-02-02 23:14:27 +0300

    master

--- src/Content/Text/Text.js
+++ src/Content/Text/Text.js
@@ -15,50 +15,50 @@ class Text extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      level1: ([ {introductory:'Привет! "Информация о блоке". Мы пойдем от простого к сложному "Первый блок"'},
-      {taskDescription: 'Помести преступника в клетку'},
-      {task: {justifyContent: 'flex-start'}},
-      {infoElement: [['justify-content ~ свойство которое отвечает за расположение элементов по оси'],['justify-content: centr; ~ располагает елемент в центре блока']] }
-    ]) 
-      };
-    
+      level: ([{ introductory: 'Привет! "Информация о блоке". Мы пойдем от простого к сложному "Первый блок"' },
+      { taskDescription: 'Помести преступника в клетку' },
+      { task: { justifyContent: 'flex-start' } },
+      { infoElement: [['justify-content ~ свойство которое отвечает за расположение элементов по оси'], ['justify-content: centr; ~ располагает елемент в центре блока']] }
+      ])
+    };
+
 
   };
 
 
-   test = (inpt) => { // получаю из инпута текс, отдаю в стейт
-    const newDisplay = this.state.level1
+  test = (inpt) => { // получаю из инпута текс, отдаю в стейт
+    const newDisplay = this.state.level
     newDisplay[2].task = (inpt)
     this.setState({
-      level1: newDisplay
+      level: newDisplay
     })
   }
 
 
   test2 = () => {
-     // изменяю текст из стейта в подходящий для style jsx, передаю вверх готовый стиль
-    let valueInpt = this.state.level1[2].task.split(':')
-    let key = valueInpt[0].split('-') 
+    // изменяю текст из стейта в подходящий для style jsx, передаю вверх готовый стиль
+    let valueInpt = this.state.level[2].task.split(':')
+    let key = valueInpt[0].split('-')
       .map(
         (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-       )
-       .join('') 
+      )
+      .join('')
     let value = valueInpt[1]
-    const obj={};
+    const obj = {};
     obj[key] = value;
-  this.props.dataStyle(obj)
-    console.log(this.state.level1[3].infoElement)
+    this.props.dataStyle(obj)
+    console.log(this.state.level)
   }
 
 
   render() {
-     
+
     return (
       <div className="text-conteiner">
-        <HeaderText introductory = {this.state.level1[0].introductory}/>
+        <HeaderText introductory={this.state.level[0].introductory} />
         <ButtonText />
-        <InfoLevel infoElement={this.state.level1[3].infoElement}  taskDescription={this.state.level1[1].taskDescription}/>
-        <Pre test={this.test}   />
+        <InfoLevel infoElement={this.state.level[3].infoElement} taskDescription={this.state.level[1].taskDescription} />
+        <Pre test={this.test} />
         <StartButton test={this.test2} />
       </div>
     );
