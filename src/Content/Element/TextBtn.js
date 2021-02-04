321177410c82399719262af64d3d4f7d50fb503e
PavelVLG <pavelvlg4@mail.ru>
2021-02-04 20:04:23 +0300

    vs

--- src/Content/Element/TextBtn.js
+++ src/Content/Element/TextBtn.js
@@ -1,33 +1,25 @@
 import React from 'react';
 
 import '../Element/TextBtn.css';
- 
 
 
 
-class ButtonText extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: 'Text'
-        }
-    };
+const ButtonText = () => {
 
-    render() {
-        return (
-            <div className="text-button__conteiner">
-                <div className="text-flex">
-                    <div className="button__text"><p>Уровень</p></div>
-                    <div className="batton-item">
-                        <div ><button className="button--1" type="button"
-                            onClick={this.props.setLevelDown}>НАЗАД</button></div>
-                        <div ><button className="button--2" type="button" onClick={this.props.setLevelUp}>вперед</button></div>
-                        <div ></div>
-                    </div>
+    return (
+        <div className="text-button__conteiner">
+            <div className="text-flex">
+                <div className="button__text"><p>Уровень</p></div>
+                <div className="batton-item">
+                    <div ><button className="button--1" type="button"
+                        onClick={this.props.setLevelDown}>НАЗАД</button></div>
+                    <div ><button className="button--2" type="button" onClick={this.props.setLevelUp}>вперед</button></div>
+                    <div ></div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+
 };
 
 export default ButtonText;
