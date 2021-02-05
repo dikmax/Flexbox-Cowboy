089271be322486d32269eff98621fd9088ad796a
PavelVLG <pavelvlg4@mail.ru>
2021-02-05 17:29:54 +0300

    vs

--- src/Content/Display/Display.js
+++ src/Content/Display/Display.js
@@ -9,17 +9,18 @@ class Display extends React.Component {
     super(props);
     this.state = {
       blockCowboy: 'Man',
-      blockСell: [{ margin: '4px 353px'}]
+      blockСell: [{ margin: '4px 353px'},{ float: 'left'},{ margin: 'rite'}]
     }
   };
 
-
+  // style={blockСell[2]}
   render() {
    const {blockСell} = this.state;
     return (
       <div className="display-conteiner " style={this.props.itemDisplay} >
+           <div className="display-item--prison" ></div> 
         <div className="display-item--cowboy"></div>
-        <div className="display-item--prison" style={blockСell[0]}></div> 
+     
       </div>
     );
   }
