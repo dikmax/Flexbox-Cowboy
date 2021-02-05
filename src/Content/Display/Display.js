f6578fdf0b1a4fe9f712a7f5c571ac12ab54b912
PavelVLG <pavelvlg4@mail.ru>
2021-02-05 13:03:35 +0300

    vs

--- src/Content/Display/Display.js
+++ src/Content/Display/Display.js
@@ -4,21 +4,25 @@ import '../Display/Display.css'
 
 
 
-class Display extends React.Component{
-  constructor(props){
+class Display extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-    data: 'Display'
-  }};
+      blockCowboy: 'Man',
+      blockСell: 'Prison'
+    }
+  };
 
- 
-render(){
-  return (
-    <div className="display-conteiner " style={this.props.itemDisplay} >
-     <div className="display-item"></div>
-    </div>
-  );
-} };
+
+  render() {
+    return (
+      <div className="display-conteiner " style={this.props.itemDisplay} >
+        <div className="display-item--cowboy"></div>
+         <div className="display-item--prison"><div><p>Клетка</p></div></div>
+      </div>
+    );
+  }
+};
 
 export default Display;
 
