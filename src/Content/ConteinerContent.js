174777bfe70ccd172ca85891e266ab29f927bf8f
PavelVLG <pavelvlg4@mail.ru>
2021-02-07 14:37:40 +0300

    vs

--- src/Content/ConteinerContent.js
+++ src/Content/ConteinerContent.js
@@ -17,13 +17,13 @@ class ConteinerContent extends React.Component {
         { taskDescription: 'Помести преступника в клетку(центр).' },
         { task: { justifyContent: 'flex-start' } },
         { infoElement: [['justify-content ~ свойство которое отвечает за расположение элементов по оси X.'], ['justify-content: center; ~ располагает елемент в центре блока.']] },
-        { locationCells: { justifyContent: 'center' } }, { id: 1 }],
+        { locationCells: { justifyContent: 'center' } }, { id: 1 }, ],
 
         [{ introductory: 'Начало положено. Как известно "Дорогу осилит идущий".' },
         { taskDescription: 'Теперь помести преступника в клету справа.' },
         { task: { justifyContent: 'flex-start' } },
         { infoElement: [['justify-content: center; ~ располагает елемент в центре блока.'], ['justify-content: flex-end; ~ располагает элемент в правом углу по оси X.']] },
-        { locationCells: { justifyContent: 'flex-end' } }, { id: 2 }],
+        { locationCells: { justifyContent: 'flex-end' } }, { id: 2 },],
 
         [{ introductory: 'Компания, в моем лице, считает что ты молодец!' },
         { taskDescription: 'Ты знаешь, что нужно сделать.' },
@@ -58,47 +58,48 @@ class ConteinerContent extends React.Component {
   };
 
   test = (inpt) => { // получаю из инпута текс, отдаю в стейт
-    const newDisplay = this.state. newLev.slice()
-    newDisplay[newDisplay.length] = (inpt)
+    const newDisplay = this.state.newLev.slice()
+    newDisplay[6] = (inpt)
+  
     this.setState({
-      level: newDisplay
+      newLev: newDisplay
     })
-    console.log(newDisplay)
   };
 
   test2 = () => {
 
     try {
-      let clone = this.state.newLev // клонирую state
+     const clone = this.state.newLev.slice() // клонирую state
+      
     
-     const separation = clone[5]//убираю лишнии пробелы и точки с запятой
+     
+     const separation = clone[6]//убираю лишнии пробелы и точки с запятой
         .replace(/\s/g, '')
        .split(';')
         .filter(function (item) {
            return item !== "";
        });
-   console.log(separation, 'separation' )
-  //     const transform = separation.map(function (item) { // убираю лишнее и делаю заглавные буквы
+  
+      const transform = separation.map(function (item) { // убираю лишнее и делаю заглавные буквы
 
-  //       return (item.split(':')[0])
-  //         .split('-')
-  //         .map(
-  //           (word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)
-  //         ).join('') + ':' + item.split(':')[1]
-  //     });
+        return (item.split(':')[0])
+          .split('-')
+          .map(
+            (word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)
+          ).join('') + ':' + item.split(':')[1]
+      });
 
-  //     console.log(transform, 'transform' )
 
-  //     const obj = transform.map(function (item) { // делаю подходящий для JSX формат.
-  //       const newObj = {};
-  //       newObj[item.split(':')[0]] = item.split(':')[1]
-  //       return newObj;
-  //     })
-  //     console.log(obj, 'transform')
-      
-   
+      const obj = transform.map(function (item) { // делаю подходящий для JSX формат.
+        const newObj = {};
+        newObj[item.split(':')[0]] = item.split(':')[1]
+        return newObj;
+      })
+      // clone[2].task,
     
-
+     
+      console.log({...obj})
+  
     }
 
     catch {
