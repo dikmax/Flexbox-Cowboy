654d913ab90c572736d3c6319a653677890bebde
PavelVLG <pavelvlg4@mail.ru>
2021-02-16 14:31:26 +0300

    vs

--- src/Content/Element/PopUp/PopUp.js
+++ src/Content/Element/PopUp/PopUp.js
@@ -2,25 +2,27 @@ import React from 'react'
 import './PopUp.css'
 
 
+
 const PopUp = ({ levelCompleted } = this.props) => {
 
-const classes = ['modal']
-if (levelCompleted !== true) {
-  classes.push('active')
-} 
-const test = (e) => {
-let a = 5;
- console.log(e.target)
-}
- 
-  
+  const classes = ['modal']
+  if (levelCompleted !== true) {
+    classes.push('active')
+  }
+  function test() {
+
+     classes.pop()
+     console.log(classes)
+
+
+  }
 
   return (
     <div className={classes.join(' ')}>
       <div className='modal__content'>
         <div className="modal__text"><p> <span>Джо cбежал</span></p></div>
       </div>
-      <div className="modal__close"><button className="close__btn" onClick={test}>X</button></div>
+      <div className="modal__close"><button className="close__btn" onClick={() => test()}>X</button></div>
     </div>
   )
 }
