90a3a34cbd584f7c6627b86c3f29d9e140e79a8b
PavelVLG <pavelvlg4@mail.ru>
2021-02-16 17:42:16 +0300

    vs

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,27 +1,89 @@
+import React from 'react'
 import './Navigation.css';
 import Music from "./ugly.mp3"
 
-function Navigation() {
 
-  let music = new Audio(Music)
 
-  function playMusic() {
-    music.play()
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usePlay: false,
+      text: 'ВКЛючить атмосферу'
+    }
+  };
+
+  newState = () => {
+
+    this.setState({
+      usePlay: (!this.state.usePlay)
+    })
+    if (this.state.usePlay == true) {
+      this.setState({
+        text: 'ВЫКЛючить атмосферу'
+      })
+    } else {
+      this.setState({
+        text: 'ВКЛючить атмосферу'
+      })
+    }
+
   }
-  function stopMusic() {
-    music.pause()
+  sd = () => {
+    let music = new Audio(Music);
+    if (this.state.usePlay == true) {
+      music.play()
+      this.setState({
+        text: 'ВЫКЛючить атмосферу'
+      })
+    } else {
+      music.pause()
+      this.setState({
+        text: 'ВКЛючить атмосферу'
+      })
+    }
   }
 
-
-  return (
-    <nav className="header">
-      <div className="logo"><p>Pavel<span className="logo-span">VLG</span>4</p></div>
-      <div className="header__conteyner-flex">
-        <div className="header__item"><p onClick={playMusic}>Включить атмоссферу</p></div>
-        <div className="header__item"><p onClick={stopMusic}>Выключить атмосферу</p></div>
-      </div>
-    </nav>
-  );
-}
+  render() {
+    const { text } = this.state
+    const { newState } = this
+    return (
+      <nav className="header">
+        <div className="logo"><p>Pavel<span className="logo-span">VLG</span>4</p></div>
+        <div className="header__conteyner-flex">
+          <div className="header__item"><p onClick={newState}>{text}</p></div>
+        </div>
+      </nav>
+    );
+  }
+};
 
 export default Navigation;
+
+
+
+// function Navigation() {
+
+//   let music = new Audio(Music)
+
+//   function playMusic() {
+//     music.play()
+//   }
+//   function stopMusic() {
+//     music.pause()
+//   }
+
+
+//   return (
+//     <nav className="header">
+//       <div className="logo"><p>Pavel<span className="logo-span">VLG</span>4</p></div>
+//       <div className="header__conteyner-flex">
+//         <div className="header__item"><p onClick={playMusic}>Включить атмоссферу</p></div>
+//         <div className="header__item"><p onClick={stopMusic}>Выключить атмосферу</p></div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navigation;
