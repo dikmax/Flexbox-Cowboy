5f037bae9bdbd59c50b0fd948e543a5487187609
PavelVLG <pavelvlg4@mail.ru>
2021-02-17 12:25:59 +0300

    мы

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,55 +2,43 @@ import React from 'react'
 import './Header.css';
 import Music from "./ugly.mp3"
 
-
-
-
 class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       usePlay: false,
-      text: 'ВКЛючить атмосферу'
+      text: 'Включить атмосферу'
     }
   };
 
-  audio = new Audio(Music)
+  audio = new Audio(Music);
 
-
-  newState = () => {
-
+  playMusic = () => {
     this.setState({ usePlay: !this.state.usePlay }, () => {
       if (this.state.usePlay === true) {
         this.setState({
-          text: 'ВЫКЛючить атмосферу'
+          text: 'Выключить атмосферу'
         })
         this.audio.play()
       } else {
         this.setState({
-          text: 'ВКЛючить атмосферу'
+          text: 'Включить атмосферу'
         })
         this.audio.pause()
       }
-    })
-    this.play()
-  };
-
-  play = ({ usePlay } = this.state) => {
-
-    if (usePlay === true) {
-      console.log(this.audio)
     }
-  }
-
+   )
+  };
 
   render() {
     const { text } = this.state
-    const { newState } = this
+    const { playMusic } = this
+
     return (
       <nav className="header">
         <div className="logo"><p>Pavel<span className="logo-span">VLG</span>4</p></div>
         <div className="header__conteyner-flex">
-          <div className="header__item"><p onClick={newState}>{text}</p></div>
+          <div className="header__item"><p onClick={playMusic}>{text}</p></div>
         </div>
       </nav>
     );
@@ -59,29 +47,3 @@ class Header extends React.Component {
 
 export default Header;
 
-
-
-// function Navigation() {
-
-//   let music = new Audio(Music)
-
-//   function playMusic() {
-//     music.play()
-//   }
-//   function stopMusic() {
-//     music.pause()
-//   }
-
-
-//   return (
-//     <nav className="header">
-//       <div className="logo"><p>Pavel<span className="logo-span">VLG</span>4</p></div>
-//       <div className="header__conteyner-flex">
-//         <div className="header__item"><p onClick={playMusic}>Включить атмоссферу</p></div>
-//         <div className="header__item"><p onClick={stopMusic}>Выключить атмосферу</p></div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navigation;
