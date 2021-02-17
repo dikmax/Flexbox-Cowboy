62f9dae114b218e634c51ac5d10885026120cfc9
PavelVLG <pavelvlg4@mail.ru>
2021-02-17 16:38:39 +0300

    vs

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,12 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footer" ><div ><p>Подвал</p></div></div>
+    <div className="footer" ><div className="footer__flex"  >
+      <div className="telegram"><a target="__blanc" href="https://t.me/PavelVLG4">teleg</a></div>
+      <div className="mail"><a target="__blanc" href="#">mail</a></div>
+      <div className="facebook"><a target="__blanc" href="#">face</a></div>
+      <div className="instagram"><a target="__blanc" href="https://www.instagram.com/pavel.b.vlg">insta</a></div>
+    </div></div>
   );
 }
 export default Footer;
