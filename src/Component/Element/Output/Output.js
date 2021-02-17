17c026c99d992d938ebd20e595d6904b9de91f04
PavelVLG <pavelvlg4@mail.ru>
2021-02-17 15:20:05 +0300

    vs

--- src/Component/Element/Output/Output.js
+++ src/Component/Element/Output/Output.js
@@ -3,13 +3,15 @@ import './Output.css';
 
 const Output = ({ userInpt }) => {
 
+
     return (
         <div className="exit-conteiner">
             <p className="exit--befor">  .class &ensp;&#123; <br />display: flex; </p>
             <textarea className="exit__input"
-                placeholder="Поле для написания кода"
+                placeholder='поле ввода'
                 rows="4"
                 onChange={(e) => { userInpt(e.target.value) }}
+                id="txtarea"
             ></textarea>
             <p className="exit--after"> &#125;</p>
         </div>)
