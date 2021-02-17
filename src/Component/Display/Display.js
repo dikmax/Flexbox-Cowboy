b6a958370a6042d440f7b9bffae499b6a785e4b7
PavelVLG <pavelvlg4@mail.ru>
2021-02-17 12:29:37 +0300

    мы

--- src/Component/Display/Display.js
+++ src/Component/Display/Display.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './Display.css'
 
-
-
-
-const Display = ({  displayPrison,displayCowboy} = this.props) => {
-
-
+const Display = ({ displayPrison, displayCowboy }) => {
 
   return (
     <div className="display-conteiner " style={displayCowboy} >
       <div className="display-item--cowboy"></div>
-      <div className="display-item" style={ displayPrison} > <div className="display-item--prison"></div></div>
-
+      <div className="display-item" style={displayPrison} > <div className="display-item--prison"></div></div>
     </div>
   );
 
